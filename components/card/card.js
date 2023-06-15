896ac4eabffd37f2725285c5c5134972914e6637
abiola1864 <oyebanjoabiola@gmail.com>
2023-06-15 12:34:25 +0200

    fetch character added

--- components/card/card.js
+++ components/card/card.js
@@ -5,17 +5,9 @@
 //     console.log(data);
 //     const character = data.results;
 //   }
-async function fetchData() {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
-    const data = await response.json();
-    console.log(data);
-    const character = data.results;
-  }
-  
-    
-  
 
-export function createCharacterCard(character) {
+  
+export default function createCharacterCard(character) {
 
 //   const cardContainer = document.querySelector('[data-js="card-container"]');
   const card = document.createElement("li");
@@ -30,14 +22,14 @@ export function createCharacterCard(character) {
   <div class="card__image-gradient"></div>
 </div>
 <div class="card__content">
-  <h2 class="card__title">${character.name[1]}Rick Sanchez$</h2>
+  <h2 class="card__title">${character.name}</h2>
   <dl class="card__info">
     <dt class="card__info-title">Status</dt>
-    <dd class="card__info-description">Alive</dd>
+    <dd class="card__info-description">${character.status}</dd>
     <dt class="card__info-title">Type</dt>
-    <dd class="card__info-description"></dd>
+    <dd class="card__info-description"><${character.type}/dd>
     <dt class="card__info-title">Occurrences</dt>
-    <dd class="card__info-description">51</dd>
+    <dd class="card__info-description">${character.episode.length}</dd>
   </dl>
 </div>
 `;
